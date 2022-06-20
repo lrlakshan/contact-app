080891070437771070700e8ea585323efcb54e1c
lakshan.rathnayaka <lrlakshan@gmail.com>
2022-06-20 12:46:21 +0530

    initial commit

---
+++ src/Components/ContactCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContactCard = (props) => {
+
+    const {Id, name, email} = props.contact;
+
+    return (
+        <div className="item">
+            <div className="content">
+                <div className="header">{name}</div>
+                <div>{email}</div>
+            </div>
+            <i className="trash alternate outline icon"></i> 
+        </div>
+    );
+}
+
+export default ContactCard;
