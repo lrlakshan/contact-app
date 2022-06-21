0f4ebf3b5bcb077217bbeafd6aeb15a2211c499e
lakshan.rathnayaka <lrlakshan@gmail.com>
2022-06-21 18:53:29 +0530

    Developed local storage of contacts

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -6,12 +6,26 @@ import ContactList from "./ContactList";
 
 const App = () => {
 
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   // Function to add new contacts to contacts array
   const addContactHandler = (contact) => {
     setContacts([...contacts, contact]);
   }
+
+  // Only calls in the initial render with the contact data on the local storage
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts.length > 0) {
+      setContacts(retrieveContacts);
+    }
+  }, []);
+
+  // Calls whenever the contacts state is changed
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div>
